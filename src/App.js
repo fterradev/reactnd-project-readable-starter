e4825f301fb4ac7b460fba8ea96bf7a083c03546
Fernando Terra <nandobrt@gmail.com>
2017-11-16 17:24:16 -0200

    feat: Add draft home ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-//import './App.css';
-import {Toolbar, ToolbarRow, ToolbarTitle, Fab} from 'rmwc';
+import './App.css';
+import {Toolbar, ToolbarRow, ToolbarTitle, Fab, Card, CardMedia, CardPrimary, CardTitle, CardSubtitle, CardSupportingText, CardActions, CardAction, Typography, Button, IconToggle} from 'rmwc';
 
 class App extends Component {
   render() {
@@ -11,8 +11,68 @@ class App extends Component {
           <ToolbarRow>
             <ToolbarTitle>Toolbar</ToolbarTitle>
           </ToolbarRow>
-          <Fab>add</Fab>
         </Toolbar>
+        <Fab className="app-fab app-fab--absolute">add</Fab>
+        <Card>
+          <CardPrimary>
+            <CardTitle large>Post title</CardTitle>
+            <CardSubtitle>2017/11/16 by Fernando Terra</CardSubtitle>
+          </CardPrimary>
+          <CardSupportingText>
+            <Typography use="button">5 votes</Typography>&nbsp;|&nbsp;
+            <Typography use="button">2 comments</Typography>
+            
+          </CardSupportingText>
+          {/* <CardActions className="mdc-card__actions--vertical"> */}
+          <CardActions>
+            <CardAction>
+              <i className="material-icons mdc-button__icon">arrow_drop_up</i>
+              Upvote
+            </CardAction>
+            <CardAction>
+            <i className="material-icons mdc-button__icon">arrow_drop_down</i>
+              Downvote
+            </CardAction>
+            <CardAction>
+            <i className="material-icons mdc-button__icon">edit</i>
+              Edit
+            </CardAction>
+            <CardAction>
+            <i className="material-icons mdc-button__icon">delete</i>
+              Remove
+            </CardAction>
+          </CardActions>
+        </Card>
+        <Card>
+          <CardPrimary>
+            <CardTitle large>Post title</CardTitle>
+            <CardSubtitle>2017/11/16 by Fernando Terra</CardSubtitle>
+          </CardPrimary>
+          <CardSupportingText>
+            <Typography use="button">5 votes</Typography>&nbsp;|&nbsp;
+            <Typography use="button">2 comments</Typography>
+            
+          </CardSupportingText>
+          {/* <CardActions className="mdc-card__actions--vertical"> */}
+          <CardActions>
+            <CardAction>
+              <i className="material-icons mdc-button__icon">arrow_drop_up</i>
+              Upvote
+            </CardAction>
+            <CardAction>
+            <i className="material-icons mdc-button__icon">arrow_drop_down</i>
+              Downvote
+            </CardAction>
+            <CardAction>
+            <i className="material-icons mdc-button__icon">edit</i>
+              Edit
+            </CardAction>
+            <CardAction>
+            <i className="material-icons mdc-button__icon">delete</i>
+              Remove
+            </CardAction>
+          </CardActions>
+        </Card>
       </div>
     );
   }
