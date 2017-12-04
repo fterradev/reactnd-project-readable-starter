aebf74720289eb3a89d8db48a63870648d5261e3
Fernando Terra <nandobrt@gmail.com>
2017-12-04 17:31:22 -0200

    feat: Add initial code for category routing

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,129 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchCategories, fetchPosts } from '../actions';
-import './App.css';
-import PostFormDialog from './PostFormDialog';
-import { Toolbar, ToolbarRow, ToolbarSection, ToolbarTitle, ToolbarMenuIcon, ToolbarIcon } from 'rmwc/Toolbar';
-import { Fab, Typography, Button } from 'rmwc';
-import { Card, CardPrimary, CardTitle, CardSubtitle, CardSupportingText, CardActions, CardAction} from 'rmwc/Card';
-import { MenuAnchor, Menu, MenuItem } from 'rmwc/Menu';
-import sortBy from 'sort-by';
+import { fetchCategories } from '../actions';
+import { Route } from 'react-router-dom';
+import Posts from './Posts'
 
 class App extends Component {
-  state = {
-    postFormDialogIsOpen: false,
-    categoriesOptions: [],
-    selectedCategory: null
-  };
-
   componentDidMount() {
     this.props.fetchCategories();
-    this.props.fetchPosts(this.state.selectedCategory);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.selectedCategory !== prevState.selectedCategory) {
-      this.props.fetchPosts(this.state.selectedCategory);
-    }
-  }
-  
   render() {
-    const { postFormDialogIsOpen, selectedCategory } = this.state;
-    const { categoriesStore, postsStore } = this.props;
-    const nullCategoryOption = {
-      name: 'All',
-      value: null
-    };
-    const categoriesOptions = [nullCategoryOption].concat(
-      categoriesStore.items.map(category => ({
-        name: category.name,
-        value: category
-      }))
-    );
-    let orderedPosts = [...postsStore.items];
-    orderedPosts.sort(sortBy('-voteScore'));
+    const { categoriesStore } = this.props;
     return (
-      <div>
-        <Toolbar>
-          <ToolbarRow>
-            <ToolbarSection alignStart>
-              <ToolbarMenuIcon use="menu"/>
-                <MenuAnchor onClick={evt => this.setState({'menuIsOpen': !this.state.menuIsOpen})} style={{display: 'flex'}}>
-                  <ToolbarTitle>{selectedCategory ? selectedCategory.name : 'All'}</ToolbarTitle>
-                  <ToolbarIcon use="arrow_drop_down" style={{paddingLeft: 0}} />
-                  <Menu
-                    open={this.state.menuIsOpen}
-                    onClose={evt => this.setState({menuIsOpen: false})}
-                    onSelected={evt => this.setState(
-                      {selectedCategory: categoriesOptions[evt.detail.index].value}
-                    )}
-                  >
-                    {categoriesOptions.map(({ name }) => 
-                      <MenuItem key={name}>{name}</MenuItem>
-                    )}
-                  </Menu>
-                </MenuAnchor>
-            </ToolbarSection>
-
-          </ToolbarRow>
-        </Toolbar>
-        <PostFormDialog isOpen={postFormDialogIsOpen} onClose={() => this.setState({postFormDialogIsOpen: false})} />
-        <Fab className="app-fab app-fab--absolute" onClick={evt => this.setState({postFormDialogIsOpen: true})}>add</Fab>
-        {orderedPosts.map(post =>
-          <Card key={post.id}>
-            <CardPrimary>
-              <CardTitle large>{post.title}</CardTitle>
-              <CardSubtitle>{(new Date(post.timestamp)).toDateString()} by {post.author}</CardSubtitle>
-            </CardPrimary>
-            <CardSupportingText>
-              <Typography use="button">{post.voteScore} votes</Typography><br />
-              <Button stroked>
-                <i className="material-icons mdc-button__icon">arrow_upward</i>
-                Upvote
-              </Button>&nbsp;
-              <Button stroked>
-                <i className="material-icons mdc-button__icon">arrow_downward</i>
-                Downvote
-              </Button>
-            </CardSupportingText>
-            <CardSupportingText>
-              <Typography use="button">{post.commentCount} comments</Typography>
-              
-            </CardSupportingText>
-            {/* <CardActions className="mdc-card__actions--vertical"> */}
-            <CardActions>
-              {/* <CardAction>
-                <i className="material-icons mdc-button__icon">arrow_upward</i>
-                Upvote
-              </CardAction>
-              <CardAction>
-              <i className="material-icons mdc-button__icon">arrow_downward</i>
-                Downvote
-              </CardAction> */}
-              <CardAction>
-              <i className="material-icons mdc-button__icon">edit</i>
-                Edit
-              </CardAction>
-              <CardAction>
-              <i className="material-icons mdc-button__icon">delete</i>
-                Remove
-              </CardAction>
-            </CardActions>
-          </Card>
-        )}
-      </div>
+      <Route
+        exact
+        path="/:category?"
+        render={({ match }) => {
+          let selectedCategory = undefined;
+          if (match.params.category) {
+            const categoryPath = match.params.category;
+            selectedCategory = categoriesStore.items.find(category => category.path == categoryPath);
+          }
+          return (
+            <Posts category={selectedCategory} />
+          );
+        }}
+      />
     );
   }
 }
 
-function mapStateToProps({ categories, posts }) {
+function mapStateToProps({ categories }) {
   return {
-    categoriesStore: categories,
-    postsStore: posts
+    categoriesStore: categories
   };
 }
 
 export default connect(mapStateToProps, {
-  fetchCategories,
-  fetchPosts
+  fetchCategories
 })(App);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -19,6 +20,8 @@ const store = createStore(
 );
 
 ReactDOM.render(<Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
