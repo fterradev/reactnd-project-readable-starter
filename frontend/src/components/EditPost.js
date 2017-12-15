29e450bf04763fdd0c0369360f79e43320e72110
Fernando Terra <nandobrt@gmail.com>
2017-12-15 15:42:03 -0200

    fix: Validate category as required
    Turns select field into CSS only MDC component, whose required attribute
    just works out of the box.

--- frontend/src/components/EditPost.js
+++ frontend/src/components/EditPost.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardPrimary, CardTitle, CardSupportingText, CardActions, CardAction} from 'rmwc/Card';
 import { TextField } from 'rmwc/TextField';
-import { Select } from 'rmwc/Select';
 import serializeForm from 'form-serialize';
 import uuidv4 from 'uuid/v4';
 
@@ -19,9 +18,6 @@ class EditPost extends Component {
       author: 'Fernando',
       timestamp: Date.now()
     };
-    if (this.props.isParent) {
-      values.category = this.categorySelect.value;
-    }
     if (this.props.onCreate) {
       this.props.onCreate(values);
     }
@@ -40,20 +36,31 @@ class EditPost extends Component {
               </CardTitle>
             </CardPrimary>
           }
-          <CardSupportingText>
-            <br />
-            {
-              isParent &&
-              <div>
-                <Select name="category"
-                  label="Category" required
-                  placeholder="Select a category"
-                  options={categoriesStore.options}
-                  value={categoryPath}
-                  apiRef={(select) => this.categorySelect = select}
-                />
+          {
+            isParent &&
+            <CardSupportingText>
+              {
+                /**
+                 * TODO: Remove css loading when updating to a rmwc version with MDC 27.
+                 */
+              }
+              <link
+                rel="stylesheet"
+                href="https://unpkg.com/material-components-web@0.27.0/dist/material-components-web.min.css" />
+              <div className="mdc-select">
+                <select className="mdc-select__surface" required="true" name="category" defaultValue={categoryPath}>
+                  <option value="">Pick a category</option>
+                  {
+                    categoriesStore.items.map(category => (
+                      <option key={category.path} value={category.path}>{category.name}</option>
+                    ))
+                  }
+                </select>
+                <div className="mdc-select__bottom-line"></div>
               </div>
-            }
+            </CardSupportingText>
+          }
+          <CardSupportingText>
             <TextField name="body" textarea fullwidth rows="8" required />
           </CardSupportingText>
           <CardActions>
